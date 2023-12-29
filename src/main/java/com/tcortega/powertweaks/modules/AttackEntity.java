9102cbc0b822f45a9c7f5608a50b0d01a49ed5e9
Tarcisio Caetano <tarcisiocaetano@jcmsystem.com.br>
2023-12-29 19:51:49 +0000

    wip: new stuff

---
+++ src/main/java/com/tcortega/powertweaks/modules/AttackEntity.java
@@ -0,0 +1,12 @@
+package com.tcortega.powertweaks.modules;
+
+import meteordevelopment.orbit.EventHandler;
+import org.lwjgl.glfw.GLFW;
+
+public class AttackEntity extends Module {
+    public AttackEntity() {
+        super("AttackEntity", GLFW.GLFW_KEY_G);
+    }
+
+    
+}
